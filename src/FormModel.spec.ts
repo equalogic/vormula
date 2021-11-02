387d6bf1de73735b1635cff352ba951a40a0ed02
Simon Garner <simon.garner@madscience.co.nz>
2021-11-03 12:40:40 +1300

    Add tests for FormModel.get and set

--- src/FormModel.spec.ts
+++ src/FormModel.spec.ts
@@ -1,15 +1,42 @@
 import { FormModel } from './FormModel';
 
 describe('FormModel', () => {
-  describe('data', () => {
-    it('Returns form field values', () => {
+  describe('set', () => {
+    it('Sets a field value', () => {
       const formModel = new FormModel({
         name: {
           label: 'Your name',
           type: 'text',
         },
       });
+      formModel.set('name', 'Joe Bloggs');
 
+      expect(formModel.fields.name.value).toEqual('Joe Bloggs');
+    });
+  });
+
+  describe('get', () => {
+    it('Gets a field value', () => {
+      const formModel = new FormModel({
+        name: {
+          label: 'Your name',
+          type: 'text',
+          value: 'Joe Bloggs',
+        },
+      });
+
+      expect(formModel.get('name')).toEqual('Joe Bloggs');
+    });
+  });
+
+  describe('data', () => {
+    it('Returns all form field values', () => {
+      const formModel = new FormModel({
+        name: {
+          label: 'Your name',
+          type: 'text',
+        },
+      });
       formModel.set('name', 'Joe Bloggs');
 
       expect(formModel.data).toEqual({ name: 'Joe Bloggs' });
